1f8330ab0285964ec6ebcc582a14fb98b604055b
asjadh-wazeer <85475496+asjadh-wazeer@users.noreply.github.com>
2021-06-07 11:22:11 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note(){
+  
+  return (
+    <div className="note">
+      <h1>Web Dev with Javascript and React.js Bootcamp</h1>
+      <p>To my favorite teacher Shaurya. You have helped me in more ways than simply enriching my basic knowledge. I will always be thankful!
+        
+        
+        Asjadh Wazeer from Sri Lanka </p>
+    </div>
+  )
+}
+
+export default Note;
